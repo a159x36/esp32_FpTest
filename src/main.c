720153fb2751ff7c2acd7bfe56d7ebd3de0b8eb5
Martin Johnson <M.J.Johnson@massey.ac.nz>
2026-09-09 12:00:26 +1200

    Update for latest idf, add pio envs for t-display-s3 and show output on lcd screen

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include <esp_timer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "fonts.h"
+#include "graphics.h"
 
 static int tv[6];
 const int N = 3200000;
@@ -41,24 +43,45 @@ TEST(float,muladdfloat,muladdops)
 TEST(double,muladddouble,muladdops)
 
 void timeit(char *name,void fn(void)) {
+    char str[64];
     vTaskDelay(1);
     tv[0]=tv[1]=tv[2]=tv[3]=tv[4]=tv[5]=1;
     // get time since boot in microseconds
     uint64_t time=esp_timer_get_time();
     unsigned ccount,icount,ccount_new;
-    RSR(CCOUNT,ccount);
-    WSR(ICOUNT, 0);
-    WSR(ICOUNTLEVEL,2);
+    RSR(XT_REG_CCOUNT,ccount);
+    WSR(XT_REG_ICOUNT, 0);
+    WSR(XT_REG_ICOUNTLEVEL,2);
     fn();
-    RSR(CCOUNT,ccount_new);
-    RSR(ICOUNT,icount);
+    RSR(XT_REG_CCOUNT,ccount_new);
+    RSR(XT_REG_ICOUNT,icount);
     time=esp_timer_get_time()-time;
     float cpi=(float)(ccount_new-ccount)/icount;
-    printf ("%s \t %f MOP/S   \tCPI=%f\n",name, (float)N/time,cpi);
+    setFontColour(255,255,255);
+    snprintf(str,64,"%s",name);
+    print_xy(str,0,LASTY+8);
+    setFontColour(0,255,0);
+    snprintf(str,64,"%.2f MOP/S",(float)N/time);
+    print_xy(str,22*5,LASTY);
+    setFontColour(0,255,255);
+    snprintf(str,64,"CPI:%.2f",cpi);
+    print_xy(str,37*5,LASTY);
+    printf ("%22s %.2f MOP/S CPI=%.2f\n",name, (float)N/time,cpi);
+    
 }
-
+extern int antialias;
 void app_main() {
+    // initialise graphics and lcd display
+    graphics_init();
+    setFont(FONT_DEJAVU24);
+    setFontColour(32,255,32);
+    cls(0);
+    gprintf("Running Benchmark");
+    flip_frame();
+    setFont(FONT_SMALL);
+    setFontColour(255,255,255);
     while(1) {
+        cls(0);
         timeit("Integer Addition",addint);
         timeit("Integer Multiply",mulint);
         timeit("Integer Division",divint);
@@ -73,5 +96,6 @@ void app_main() {
         timeit("Double Multiply", muldouble);
         timeit("Double Division", divdouble);
         timeit("Double Multiply-Add", muladddouble);
+        flip_frame();
     }
 }
